f315e4601d3fee121378e1db4769ad6506684594
Vitor Gabriel <gabrijac57@gmail.com>
2023-01-30 12:54:51 -0400

    refactor: versao final da pagina de listar produtos

--- Estudo FullStack/Frontend/src/js/scriptGetProducts.js
+++ Estudo FullStack/Frontend/src/js/scriptGetProducts.js
@@ -1,50 +1,65 @@
 //listar produtos na tela
-document.addEventListener("DOMContentLoaded", e => {
+document.addEventListener("DOMContentLoaded", async e => {
     //pega os produtos
-    const products = getProducts();
+    let products;
+    await getProducts().then(response => { products = response.produtos });
+    //captura a tabela
     const table = document.getElementById('table-products');
     table.innerHTML = '';
     //percorre o array de produtos
-    if (typeof products.value === 'object') {
-        products.forEach((product) => {
-            table.innerHTML += `
-            <tr>
-                <td>${product.name}</td>
-                <td>${product.price}</td>
-                <td>${product.quantity}</td>
-                <td>
-                    <button type="button" class="btn btn-primary" onclick="changeProduct(${product.id})>Change</button>
-                    <button type="button" class="btn btn-danger" onclick="deleteProduct(${product.id})">Delete</button>
-                </td>
-            </tr>
-        `;
-        });
-    } else {
+    if (typeof products === 'object') {
         table.innerHTML = `
+        <div class="container">
+        <table class="table table-striped table-hover" id="table-products">
+            <thead>
+                <tr>
+                    <th scope="col">Code</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Unit</th>
+                    <th scope="col">Stock</th>
+                    <th scope="col">Addressing</th>
+                    <th scope="col">Buttons</th>
+                </tr>
+            </thead>
+            <tbody>'`;
+        // loop no array de produtos do backend
+        products.forEach(product => {
+            table.innerHTML += `
+                <tr>
+                    <th scope="row">${product.code}</th>
+                    <td>${product.description}</td>
+                    <td>${product.type}</td>
+                    <td>${product.unit}</td>
+                    <td>${product.stock}</td>
+                    <td>${product.addressing}</td>
+                    <td>
+                        <a href="./update.html?id=${product.id}" class="btn btn-primary">Update</a>
+                        <a href="./delete.html?id=${product.id}" class="btn btn-danger">Delete</a>
+                    </td>
+                </tr>
+            `;
+        });
+        table.innerHTML += `</tbody></table></div>`;
+    } else {
+        table.innerHTML += `
             <div class="container text-center">
-                <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">No products found</h5>
-                    <p class="card-text">Click on the button below to create a new product</p>
-                    <a href="./create.html" class="btn btn-primary">Create</a>
-                </div>
+                
             </div>
         </div>
         `;
     }
+    return
 
     //função para pegar os produtos
-    function getProducts() {
-        const products = fetch("http://localhost:8081/rest/api/v2/products/", {
+    async function getProducts() {
+        const response = await fetch("http://192.168.199.37:8081/rest/api/v2/custom/products", {
             method: "GET",
-        }).then(response => {
-            return response.json()
-        }).then(data => {
-            return data;
-        }).catch(error => {
-            console.log(error)
-        })
-        return products;
+            headers: {
+                "Content-Type": "application/json"
+            },
+        });
+        return response.json();
     }
 
 });
